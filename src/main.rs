40f03427c8bb1c041c46de10ec384d49338bd44b
Christopher <chrispidd@outlook.com>
2021-03-16 21:24:14 +1100

    v1.0 - basic game functional

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,79 @@ use std::fs;
 use rand::Rng;
 use std::io;
 
+fn draw_hangman(count: u32) {
+    match count {
+        // Match a single value
+        1 => println!("
+     __________
+      |        |
+               |
+               |
+               |
+               |
+               |
+               |
+    ============
+        "),
+        2 => println!("
+     __________
+      |        |
+      0        |
+               |
+               |
+               |
+               |
+               |
+    ============
+        "),
+        3 => println!("
+     __________
+      |        |
+      0        |
+     /|        |
+               |
+               |
+               |
+               |
+    ============
+        "),
+        4 => println!("
+     __________
+      |        |
+      0        |
+     /|\\       |
+               |
+               |
+               |
+               |
+    ============
+        "),
+        5 => println!("
+     __________
+      |        |
+      0        |
+     /|\\       |
+    /          |
+               |
+               |
+               |
+    ============
+        "),
+        6 => println!("
+      __________
+      |        |
+      0        |
+     /|\\       |
+     / \\       |
+               |
+               |
+               |
+    ============
+        "),
+        _ => println!("Ain't special"),
+    }
+}
+
 fn main() {
     println!("Welcome to hangman!");
     let wordlist = fs::read_to_string("src/wordlist.txt")
@@ -15,15 +88,22 @@ fn main() {
     for _n in 0 .. word.len(){
         incomplete_word.push('_');
     }
+    let mut wrong_count: u32 = 1;
 
     println!("guess is: {}", incomplete_word);
-    println!("Word is: {}", word);
 
     println!("\n\n\nGuess a single letter or the whole word!");
 
     loop {
+
+        draw_hangman(wrong_count);
+
         if !incomplete_word.contains('_') {
             println!("Yay you win, the word was {}", word);
+            break;
+        }
+        if wrong_count >= 6 {
+            println!("Game over :(");
             break;
         }
 
@@ -38,12 +118,14 @@ fn main() {
         println!("You guessed: {}", guess);
 
         let mut output = String::with_capacity(word.len());
+        let mut correct_guess = false;
         for (n, real_word_letter) in word.chars().enumerate(){
             let incomplete_word_letter = incomplete_word.chars().nth(n).expect("empty string");
             let guessed_letter = guess.chars().next().expect("empty string");
 
             match real_word_letter == guessed_letter {
                 true => {
+                    correct_guess = true;
                     output.push(guessed_letter);
                 },
                 false => {
@@ -51,7 +133,9 @@ fn main() {
                 }
             }
         }
-
+        if correct_guess == false {
+            wrong_count += 1;
+        }
         incomplete_word = output; 
 
     }
